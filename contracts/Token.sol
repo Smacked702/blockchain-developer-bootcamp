5b5642fb765f739e5baf127941ef64c8921d2cf5
Rob D <rnthat.702@gmail.com>
2023-06-09 14:00:54 -0700

    9. Deposit tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -75,12 +75,12 @@ contract Token {
        address _from, 
        address _to, 
        uint256 _value
-       ) 
+    ) 
        public 
        returns(bool success)
     {
-       require(_value <= balanceOf[_from]);
-       require(_value <= allowance[_from][msg.sender]);
+       require(_value <= balanceOf[_from], 'insufficient balance');
+       require(_value <= allowance[_from][msg.sender], 'insufficient allowance');
 
        // Reset Allowance
        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
