c3aeb65f3e8226b17ac9044f2332da02dcd166c5
Rob D <rnthat.702@gmail.com>
2023-06-14 00:08:02 -0700

    11. Make orders

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,43 +6,43 @@ import "hardhat/console.sol";
 contract Token {
     string public name;
     string public symbol;
-    uint256 public decimals = 18; 
+    uint256 public decimals = 18;
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
-        address indexed from, 
-        address indexed to, 
+        address indexed from,
+        address indexed to,
         uint256 value
     );
 
     event Approval(
-        address indexed owner, 
-        address indexed spender, 
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
 
     constructor(
-        string memory _name, 
-        string memory _symbol, 
+        string memory _name,
+        string memory _symbol,
         uint256 _totalSupply
     ) {
         name = _name;
-        symbol = _symbol;   
+        symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) 
-        public 
-        returns (bool success) 
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
 
         _transfer(msg.sender, _to, _value);
-        
+
         return true;
     }
 
@@ -51,7 +51,7 @@ contract Token {
         address _to,
         uint256 _value
     ) internal {
-        require(_to != address(0));        
+        require(_to != address(0));
 
         balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
@@ -60,40 +60,36 @@ contract Token {
     }
 
     function approve(address _spender, uint256 _value)
-        public 
-        returns(bool success) 
-   {
+        public
+        returns(bool success)
+    {
         require(_spender != address(0));
 
         allowance[msg.sender][_spender] = _value;
-        
+
         emit Approval(msg.sender, _spender, _value);
         return true;
-   }
+    }
 
     function transferFrom(
-       address _from, 
-       address _to, 
-       uint256 _value
-    ) 
-       public 
-       returns(bool success)
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success)
     {
-       require(_value <= balanceOf[_from], 'insufficient balance');
-       require(_value <= allowance[_from][msg.sender], 'insufficient allowance');
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
-       // Reset Allowance
-       allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
-       // spend tokens
-       _transfer(_from, _to, _value);
+        _transfer(_from, _to, _value);
 
-       return true;
+        return true;
     }
 
 }
-
-
 
 
 
