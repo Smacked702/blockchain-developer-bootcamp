08ae96c774758c1960c36d885de913b4233b95bf
Rob D <rnthat.702@gmail.com>
2023-06-18 02:47:45 -0700

    15. UI Setup

---
+++ src/assets/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Smacked out!</h1>
+      <p>This is some placeholder text...</p>
+    </div>
+  );
+}
+
+export default App;
