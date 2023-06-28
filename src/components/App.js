4bad03b1394209677295aa8b2ecfce670f1526c8
Rob D <rnthat.702@gmail.com>
2023-06-28 15:51:56 -0700

    20. Deposit Pt1

--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,51 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import config from '../config.json';
 
-import { 
-  loadProvider, 
-  loadNetwork, 
-  loadAccount, 
+import {
+  loadProvider,
+  loadNetwork,
+  loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    // Connect Ethers to loadBlockchain 
+    // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
 
-    // Fetch current network's chainId (e.g. hardhat: 31337, goerli: 42)
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
     // Reload page when network changes
-    window.ethereum.on( 'chainChanged', () => {
+    window.ethereum.on('chainChanged', () => {
       window.location.reload()
     })
 
-    // Fetch current account & balance from Metamask when chandged
+    // Fetch current account & balance from Metamask when changed
     window.ethereum.on('accountsChanged', () => {
       loadAccount(provider, dispatch)
     })
 
     // Load token smart contracts
-    const DApp =  config[chainId].DApp
+    const DApp = config[chainId].DApp
     const mETH = config[chainId].mETH
     await loadTokens(provider, [DApp.address, mETH.address], dispatch)
-  
+
     // Load exchange smart contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
-  }
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
 
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
+  }
 
   useEffect(() => {
     loadBlockchainData()
@@ -58,7 +62,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -2,7 +2,6 @@ import { ethers } from 'ethers'
 import TOKEN_ABI from '../abis/Token.json';
 import EXCHANGE_ABI from '../abis/Exchange.json';
 
-
 export const loadProvider = (dispatch) => {
   const connection = new ethers.providers.Web3Provider(window.ethereum)
   dispatch({ type: 'PROVIDER_LOADED', connection })
@@ -12,12 +11,12 @@ export const loadProvider = (dispatch) => {
 
 export const loadNetwork = async (provider, dispatch) => {
   const { chainId } = await provider.getNetwork()
-  dispatch({ type: 'NETWORK_LOADED', chainId})
+  dispatch({ type: 'NETWORK_LOADED', chainId })
 
   return chainId
 }
 
-export const loadAccount = async (provider, dispatch) => { 
+export const loadAccount = async (provider, dispatch) => {
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
   const account = ethers.utils.getAddress(accounts[0])
 
@@ -36,11 +35,11 @@ export const loadTokens = async (provider, addresses, dispatch) => {
 
   token = new ethers.Contract(addresses[0], TOKEN_ABI, provider)
   symbol = await token.symbol()
-  dispatch({ type: 'TOKEN_LOADED_1', token, symbol })
+  dispatch({ type: 'TOKEN_1_LOADED', token, symbol })
 
   token = new ethers.Contract(addresses[1], TOKEN_ABI, provider)
   symbol = await token.symbol()
-  dispatch({ type: 'TOKEN_LOADED_2', token, symbol })
+  dispatch({ type: 'TOKEN_2_LOADED', token, symbol })
 
   return token
 }
@@ -52,10 +51,52 @@ export const loadExchange = async (provider, address, dispatch) => {
   return exchange
 }
 
+export const subscribeToEvents = (exchange, dispatch) => {
+  exchange.on('Deposit', (token, user, amount, balance, event) => {
+    dispatch({ type: 'TRANSFER_SUCCESS', event })
+  })
+}
+
+// ------------------------------------------------------------------------------
+// LOAD USER BALANCES (WALLET & EXCHANGE BALANCES)
 
 
+export const loadBalances = async (exchange, tokens, account, dispatch) => {
+  let balance = ethers.utils.formatUnits(await tokens[0].balanceOf(account), 18)
+  dispatch({ type: 'TOKEN_1_BALANCE_LOADED', balance })
 
+  balance = ethers.utils.formatUnits(await exchange.balanceOf(tokens[0].address, account), 18)
+  dispatch({ type: 'EXCHANGE_TOKEN_1_BALANCE_LOADED', balance })
 
+  balance = ethers.utils.formatUnits(await tokens[1].balanceOf(account), 18)
+  dispatch({ type: 'TOKEN_2_BALANCE_LOADED', balance })
 
+  balance = ethers.utils.formatUnits(await exchange.balanceOf(tokens[1].address, account), 18)
+  dispatch({ type: 'EXCHANGE_TOKEN_2_BALANCE_LOADED', balance })
+
+}
+
+// ------------------------------------------------------------------------------
+// TRANSFER TOKENS (DEPOSIT & WITHDRAWS)
+
+export const transferTokens =  async (provider, exchange, transferType, token, amount, dispatch) => {
+  let transaction
+
+  dispatch({ type: 'TRANSFER_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
+
+    transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+    await transaction.wait()
+    transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+
+    await transaction.wait()
+
+  } catch(error) {
+    dispatch({ type: 'TRANSFER_FAIL' })
+  }
+}
 
 
